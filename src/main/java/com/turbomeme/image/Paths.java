c7d6f4cd56cb07588123bbf3e18e587ae33fe201
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2024-04-26 17:37:44 +0300

    Use proper Maven structure for backend and frontend

--- src/main/java/com/turbomeme/image/Paths.java
+++ src/main/java/com/turbomeme/image/Paths.java
@@ -8,9 +8,10 @@ import java.io.File;
 public interface Paths
 {
   String CURRENT_DIR = System.getProperty("user.dir");
-  String BACKEND_RESOURCE_DIR = CURRENT_DIR + File.separator + "backend" + File.separator + "src"
-      + File.separator + "main" + File.separator + "resources" + File.separator;
-  String FRONTEND_DIR = CURRENT_DIR + File.separator + "frontend" + File.separator;
+  String BACKEND_RESOURCE_DIR = CURRENT_DIR + File.separator + "src" + File.separator
+      + "main" + File.separator + "resources" + File.separator;
+  String FRONTEND_DIR = CURRENT_DIR + File.separator + "src" + File.separator + "main" + File.separator
+      + "webapp" + File.separator;
   String IMAGES_DIR = FRONTEND_DIR + File.separator + "img" + File.separator + "memes" + File.separator;
   String UI_DIR = FRONTEND_DIR + File.separator + "img" + File.separator + "ui" + File.separator;
 }
