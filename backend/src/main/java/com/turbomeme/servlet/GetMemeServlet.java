bd33c3e60e796dd47342d8b4eedac4691cd6fca9
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2024-04-26 16:18:38 +0300

    Fix package names

--- backend/src/main/java/com/turbomeme/servlet/GetMemeServlet.java
+++ backend/src/main/java/com/turbomeme/servlet/GetMemeServlet.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
